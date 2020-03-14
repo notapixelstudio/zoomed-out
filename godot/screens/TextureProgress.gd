49c22375c6b4a4e47f8045815b9ff69d9cd180df
SalvoB <salvob41@users.noreply.github.com>
2020-03-14 23:52:27 +0100

    Ui (#1)
    * added ui

---
+++ godot/screens/TextureProgress.gd
@@ -0,0 +1,15 @@
+extends TextureProgress
+
+var timer = 0.0
+export var rate : float = 1.0
+func _process(delta):
+	timer += delta
+	if timer >= rate:
+		timer -= rate
+		decrease_bar(1)
+	
+func decrease_bar(quantity):
+	value -= quantity
+
+func increase_bar(quantity):
+	value += quantity
