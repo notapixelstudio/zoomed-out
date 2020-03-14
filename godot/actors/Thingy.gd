08cb7b3983ff21399d8152a93c8f63b30b96b3dc
nitaku <matteo.abrate@gmail.com>
2020-03-14 02:47:57 +0100

    Decoloring

--- godot/actors/Thingy.gd
+++ godot/actors/Thingy.gd
@@ -1,15 +1,22 @@
 extends Area2D
 
+onready var animation_player = $AnimationPlayer
+
 var colored = false
 
 signal touched
 
 func _on_Thingy_body_entered(body):
 	if colored:
-		queue_free()
+		decolor()
 		emit_signal('touched')
 		
 func color():
 	colored = true
 	modulate = Color(1,0,0,1)
+	animation_player.play("blink")
+
+func decolor():
+	colored = false
+	modulate = Color(1,1,1,1)
 	
