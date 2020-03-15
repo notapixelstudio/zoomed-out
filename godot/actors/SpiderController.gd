bf3fbe04c3a0f0a2f27854a9758ec428d4c11594
salvob <tholalak@yahoo.it>
2020-03-15 23:10:56 +0100

    actor spider ready with bullet

---
+++ godot/actors/SpiderController.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+
+class_name SpiderController
+
+
+export var bullet_scene : PackedScene
+
+const speed = 200
+onready var anim = $AnimatedSprite
+onready var debug = $Debug
+
+func shoot():
+	# logic for shooting, with pattern
+	pass
