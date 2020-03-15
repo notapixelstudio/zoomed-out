d59405c5843a882e5eb7c62e3f70608f2f131929
salvob <tholalak@yahoo.it>
2020-03-15 23:12:44 +0100

    better bulletin

--- godot/actors/SpiderController.gd
+++ godot/actors/SpiderController.gd
@@ -5,10 +5,15 @@ class_name SpiderController
 
 export var bullet_scene : PackedScene
 
+signal spawn_bullet
 const speed = 200
 onready var anim = $AnimatedSprite
 onready var debug = $Debug
 
-func shoot():
+func shoot(how_many):
+	for i in how_many:
+		var bullet = bullet_scene.instance()
+		emit_signal("spawn_bullet", bullet)
+		# will be taken care from World.
 	# logic for shooting, with pattern
-	pass
+	return
