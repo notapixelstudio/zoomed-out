ed0773b87984cd5b1008fbdc7ce18c13c361f035
nitaku <matteo.abrate@gmail.com>
2020-03-16 04:29:06 +0100

    obstacles and scorpion pattern

--- godot/actors/scorpion/Scorpion.gd
+++ godot/actors/scorpion/Scorpion.gd
@@ -2,8 +2,14 @@ extends KinematicBody2D
 
 class_name Lion
 
-export var speed = Vector2(60,0)
+export var speed = Vector2(100,0)
+
+var factor = 1.0
 
 func _process(delta):
-	move_and_slide(speed)
-	
+	move_and_slide(speed*factor)
+	if factor > 0.05:
+		factor *= 0.98
+	else:
+		factor = 1.0
+		
